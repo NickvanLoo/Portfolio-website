fb8e28f58d815011fda02d90d414525e13a1a398
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-09-08 17:44:40 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ var request = new XMLHttpRequest()
 //Open a new connection, using GET request on the url endpoint.
 request.open('GET', 'https://api.github.com/users/NickvanLoo/repos', true)
 
-request.onload = function (){
+request.onload = function () {
     var data = JSON.parse(this.response);
-    
+
     var statusHTML = '';
 
-    $.each(data, function(i, status){
+    $.each(data, function (i, status) {
         statusHTML += '<tr>';
         statusHTML += '<td>' + status.name + '</td>';
         statusHTML += '<td><a target="_blank" href="' + status.html_url + '">' + status.html_url + '</a></td>';
